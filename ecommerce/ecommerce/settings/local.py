72fc127a5332af160424a79d6d0cae459b84f5d0
Artur <a.drogomeretskiy@gmail.com>
2024-09-01 21:53:42 +0300

    nc

---
+++ ecommerce/ecommerce/settings/local.py
@@ -0,0 +1,6 @@
+from .base import *
+
+# The idea is to cut settings from base settings file if it needs to be changed.
+# And change it here.
+# Also in manage.py file we can make the condition which settings to use depending on the DEBUG status.
+
